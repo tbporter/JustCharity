abba7b310426d8a9b4e627a43034d8b47dc83296
tbporter <travis.porter@gmail.com>
2014-06-01 09:38:36 -0600

    initial commit

---
+++ index_bak.php
@@ -0,0 +1,336 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	
+<script type="text/javascript" src="http://www.provableproperty.com/static/js/bitcoinprices.js"></script>
+    <link rel="shortcut icon" href="img/favicon.ico">
+
+    <title>JustCharity.tk&reg; - Transparent bounty rewards for charity</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+  </head>
+<!-- NAVBAR
+================================================== -->
+  <body>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand logo1" href="#">JustCharity</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+           
+                <li><a class="navfonts" data-toggle="modal"  href="#modal" role="button" data-target=".bs-example-modal-lg">Earn</a></li>
+                <li><a class="navfonts" data-toggle="modal"  href="#modal" role="button" data-target=".bs-example-modal-lg2">Support</a></li>
+                <li><a class="navfonts" data-toggle="modal"  href="#modal" role="button" data-target=".bs-example-modal-lg3">Learn</a></li>
+ 
+                
+                
+                <!--<li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li class="divider"></li>
+                    <li class="dropdown-header">Nav header</li>
+                    <li><a href="#">Separated link</a></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>-->
+              </ul>
+              <ul class="nav navbar-nav navbar-right" style="margin-right:10px; border-color:transparent;">
+            <li><a href="#"><img src="img/fb.png"></a></li>
+             <li><a href="#"><img src="img/g.png"></a></li>
+             <li><a href="#"><img src="img/tw.png"></a></li>
+          </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+
+   <!-- Carousel
+    ================================================== -->
+    <div id="myCarousel" class="carousel slide " data-ride="carousel">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner ">
+        <div class="item active">
+         
+          <div class="container">
+            <div class="carousel-caption"> 
+              <h1>Earn.</h1>
+              <p>Earn Bitcoin for by providing publicity to your favorite charity.</p>
+              <p><a class="btn btn-lg btn-primary" href="#earn" role="button" data-toggle="modal"  href="#modal"  data-target=".bs-example-modal-lg">Make Money Now</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+         
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Support.</h1>
+              <p>Donate and support your favorite charity</p>
+              <p><a class="btn btn-lg btn-primary" href="#earn" role="button" data-toggle="modal"  href="#modal"  data-target=".bs-example-modal-lg2">Support the Cause</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+          
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>Learn</h1>
+              <p>Learn more about the project and the team behind it.</p>
+              <p><a class="btn btn-lg btn-primary" href="#earn" role="button" data-toggle="modal"  href="#modal"  data-target=".bs-example-modal-lg3">Learn about it</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" ></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+      <div class="footer1">.</div>
+    </div><!-- /.carousel -->
+
+
+
+    <!-- Marketing messaging and featurettes
+    ================================================== -->
+    <!-- Wrap the rest of the page in another container to center all the content. -->
+
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="img-circle" src="img/cash-logo.png" alt="Generic placeholder image">
+          <h2>Earn.</h2>
+          <p>Get paid for helping out your favorite charity by spreading the word. Your publicity will help out the cause and you get compensation for your work. </p>
+          <p><a class="btn btn-default" data-toggle="modal"  href="#modal" role="button" data-target=".bs-example-modal-lg">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="img/support-logo.png" alt="Generic placeholder image">
+          <h2>Support.</h2>
+          <p>Give Bitcoin and track your donations to see how they are being spent. Now you have the chance to see where you money is going and decide if the charity is right for you.</p>
+           <p><a class="btn btn-default" data-toggle="modal"  href="#modal" role="button" data-target=".bs-example-modal-lg2">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-circle" src="img/HeadIconTeamLeftFace.png" alt="Generic placeholder image">
+          <h2>Learn.</h2>
+          <p>Gone are the days of a charity using 80% of donations to pay salaries and leaving the remaining 20% of the funds to go towards the cause. </p>
+         <p><a class="btn btn-default" data-toggle="modal"  href="#modal" role="button" data-target=".bs-example-modal-lg3">View details &raquo;</a></p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+
+
+
+<!-- Large modal -->
+
+
+<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg">
+    <div class="modal-content" style="text-align:center;">
+      <iframe src="https://docs.google.com/forms/d/16nxEzy6Nyn2wZSSomPUiW2N3V_8yfyOnwRDaAekEy1I/viewform?embedded=true" width="600" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+    </div>
+  </div>
+</div>
+<!---- end -->
+
+
+
+
+
+<!-- Support modal -->
+
+
+<div class="modal fade bs-example-modal-lg2" tabindex="1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg2">
+    <div class="modal-content" style="text-align:center; width:760px; ">
+    
+<?php
+
+//Gets the balance of the donation address from blockchain.info
+
+
+$json = file_get_contents('http://blockchain.info/address/18YUorfUBToVQzrBgQrSZU3tmk8dvt8C2R?format=json');
+$obj = json_decode($json);
+//echo $obj;final_balance;
+
+$addressBal = $obj->final_balance;
+$inBtc = $addressBal * .00000001;
+
+
+?>
+
+
+
+<script>
+    $(document).ready(function() {
+        bitcoinprices.init({
+
+            // Where we get bitcoinaverage data
+            url: "https://api.bitcoinaverage.com/ticker/all",
+
+            // Which of bitcoinaverages value we use to present prices
+            marketRateVariable: "24h_avg",
+
+            // Which currencies are in shown to the user
+            currencies: ["BTC", "USD", "EUR", "CNY"],
+
+            // Special currency symbol artwork
+            symbols: {
+                "BTC": "<i class='fa fa-btc'></i>"
+            },
+
+            // Which currency we show user by the default if
+            // no currency is selected
+            defaultCurrency: "USD",
+
+            // How the user is able to interact with the prices
+            ux : {
+                // Make everything with data-btc-price HTML attribute clickable
+                clickPrices : true,
+
+                // Build Bootstrap dropdown menu for currency switching
+                menu : true,
+            },
+
+            // Allows passing the explicit jQuery version to bitcoinprices.
+            // This is useful if you are using modular javascript (AMD/UMD/require()),
+            // but for most normal usage you don't need this
+            jQuery: jQuery,
+
+            // Price source data attribute
+            priceAttribute: "data-btc-price",
+
+            // Price source currency for data-btc-price attribute.
+            // E.g. if your shop prices are in USD
+            // but converted to BTC when you do Bitcoin
+            // checkout, put USD here.
+            priceOrignalCurrency: "BTC"
+
+        });
+    });
+</script>
+
+
+<h2>Support Charity</h2>
+For the first time in history you can support a charity and THEN see exactly where 100% of the money goes. NO hidden fees, NO paying big salaries.<br> Finally all your donations go to supporting the cause and not paying the salary of a 1% er.<br><br><b> Donate to this bitcoin address: 18YUorfUBToVQzrBgQrSZU3tmk8dvt8C2R </b>
+
+
+<h2>Project Balance</h2>
+
+<span data-btc-price="<?
+
+//echo "<h2>";
+echo $inBtc;
+//echo " BTC</h2>"; 
+
+?>">10.4543 BTC = $6543.54 USD</span>
+
+
+
+
+
+<iframe src="https://docs.google.com/spreadsheet/pub?key=0At9gf75AVkEZdDFzZ0hoajB2N2xqcEJnNV92cEF0N1E&single=true&gid=0&output=html" width="720" height="500" frameborder="0" >Loading...</iframe>
+
+
+	
+	
+    </div>
+  </div>
+</div>
+<!---- end -->
+      
+      
+ 
+ 
+ 
+ 
+ <!-- Learn modal -->
+
+
+<div class="modal fade bs-example-modal-lg3" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg3">
+    <div class="modal-content" style="text-align:center; width:760px;">
+ <p>
+<h2>What problem are you solving?</h2>
+
+The problem we are solving is that when someone donates to a charity, where does the money go?  Charities do an awful job of showing exactly where your donations go. In fact most of the time a charity only puts 20% of your money towards the actual cause and uses the other 80% to pay it’s employees and such.  Don’t believe us, want references for these claims?...check this out: <a href=http://www.ibtimes.com/worst-charities-non-profits-spend-most-their-money-fundraising-1298829>http://www.ibtimes.com/worst-charities-non-profits-spend-most-their-money-fundraising-1298829</a> ...granted these are the worst of the worst, but even a beloved American Red Cross spends tons of your donations on paying it’s employees well, for example:<br><br>
+
+“...Marsha J. Evans, President and CEO of the American Red Cross... for her salary for the year ending in 2009 was $651,957 plus expenses. Enjoys 6 weeks - fully paid holidays including all related expenses during the holiday trip for her and her husband and kids. including 100% fully paid health & dental plan for her and her family, for life. This means out of every dollar they bring in, about $0.39 goes to related charity causes.”
+Read more at <a href=http://www.snopes.com/politics/business/charities.asp#1RoSPouZYI5BBpww.99>http://www.snopes.com/politics/business/charities.asp#1RoSPouZYI5BBpww.99</a>
+<br><br>
+This project is part of the <a href=http://hack4colorado.com>Hack4Colorado</a> Hackathon competition. The team members are: 
+<br><br>
+
+<h2>What is Bitcoin?</h2>
+Bitcoin is a decentralized currency with a public ledger. We leverage this to be as transparent as possible.
+<iframe width="560" height="315" src="//www.youtube.com/embed/Gc2en3nHxA4?rel=0" frameborder="0" allowfullscreen></iframe>
+<br><br>
+
+<h2>Team</h2>
+<b>Phil Maher</b><br>
+Phil Maher has been passionate about social good and charities since 2002, when he started donating plasma for money in college to pay for books and food. His background is in software development and marketing with a degree from Illinois State University in Telecommunications Management. He has worked as a Director of Interactive Strategy for RH Donnelley and also brings real San Francisco startup experience from Ejemoni (now H5 Technologies) and Adteractive. He’s also consulted for many entrepreneurs and organizations like University of California San Francisco, Ameriteach, GameDaily, and AkimboCard.
+</p>
+    </div>
+  </div>
+</div>
+<!---- end -->     
+      
+      
+
+
+
+
+      <!-- FOOTER -->
+      <footer>
+        <!--<p class="pull-right"><a href="#">Back to top</a></p> -->
+        <p>&copy; 2014 JustCharity.tk &middot; <!--<a href="#">Privacy</a> &middot; <a href="#">Terms</a> --></p>
+      </footer>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+  </body>
+</html>
